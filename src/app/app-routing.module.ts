4e9395eeced3cb4d850708c6af1eb8480573e769
shoney.arickathil <shoney.arickathil@retromotion.com>
2024-08-19 12:20:41 +0200

    update app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,11 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { RepositoryViewerComponent } from './repository-viewer/repository-viewer.component';
+import { RepositoryListComponent } from './repository-list/repository-list.component';
 const routes: Routes = [
-  { path: ':title', component: ArticleComponent },
+  { path: 'blog:title', component: ArticleComponent },
   { path: 'project/:id', component: RepositoryViewerComponent },
+  { path: 'projects', component: RepositoryListComponent },
   { path: 'about', component: AboutComponent }, 
   { path: 'contact', component: ContactComponent },
   { path: '', component: HomeComponent },  
